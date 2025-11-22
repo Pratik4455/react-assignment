59574efb17ee9d87197eb644856d328d23dd32d1
Pratik4455 <prajapatipratik695@gmail.com>
2025-11-22 17:53:34 +0530

    react

--- src/App.js
+++ src/App.js
@@ -6,17 +6,30 @@ import MainProps from "./Assignment/MainProps";
 import Counter from "./Assignment/Counter";
 import Text from "./Assignment/Text";
 import Form from "./Assignment/Form";
+import Ulogin from "./Assignment/Ulogin";
+import Hooks from "./Assignment/Hooks";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Assignment/Home";
+import About from "./Assignment/About";
 
 function App() {
   return (
+    <BrowserRouter >
     <div className="App">
-      <FuncComp />
+      {/* <FuncComp />
       <ClassCompo />
       {/* <MainProps /> */}
-      <Counter />
+      {/* <Counter />
       <Text />
-      <Form />
+      <Form />  */}
+      {/* <Ulogin /> */}
+      {/* <Hooks /> */}
+      <Routes >
+        <Route path="/" element={<Home />}/>
+        <Route path="/About" element={<About />}/>
+      </Routes>
     </div>
+      </BrowserRouter>
   );
 }
 
